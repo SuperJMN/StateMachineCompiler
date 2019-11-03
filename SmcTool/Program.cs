b31b04a42353d988c41f0d21bc3d0b020bcad68d
José Manuel Nieto Sánchez <superjmn@outlook.com>
2019-11-03 17:17:39 +0100

    Added basic commandline compiler

---
+++ SmcTool/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.IO;
+using CommandLine;
+using Smc.Generator;
+using Smc.Optimize;
+using Smc.Semantic;
+using Smc.Syntax;
+using Smc.Tests.Implementers;
+using CommandlineParserParser = CommandLine.Parser;
+using Parser = Smc.Syntax.Parser;
+
+namespace SmcTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CommandlineParserParser.Default.ParseArguments<Options>(args)
+                .WithParsed(o => { Compile(o.InputFile, o.OutputFile); });
+        }
+
+        private static void Compile(string inputFile, string outputFile)
+        {
+            var input = File.ReadAllText(inputFile);
+            var syntax = new Parser(Tokenizer.Create()).Parse(input);
+            var semantic = new SemanticAnalyzer().Analize(syntax);
+            var machine = new Optimizer().Optimize(semantic);
+            var node = new NscGenerator().Generate(machine);
+
+            var visitor = new CsharpVisitor();
+            node.Accept(visitor);
+            
+            File.WriteAllText(outputFile, visitor.Result);
+        }
+    }
+
+    internal class Options
+    {
+        [Option('i', "InputFile")]
+        public string InputFile { get; set; }
+
+        [Option('o', "OutputFile")]
+        public string OutputFile { get; set; }
+    }
+}
